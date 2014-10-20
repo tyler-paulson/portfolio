1b55ca37d6c8471c4f9e285cfe934d5803189149
Tyler Paulson <me@tylerpaulson.com>
2014-10-20 16:39:19 -0400

    Fixes typo

--- includes/projects/tutorials.php
+++ includes/projects/tutorials.php
@@ -1,7 +1,7 @@
 <div class="project" id="milesaminute">
 	<h2>iEARN CC Tutorials</h2>
 	<p>
-		<em class="project--brief">Completed as an employee of<a href="http://us.iearn.org">iEARN-USA</a></em>
+		<em class="project--brief">Completed as an employee of <a href="http://us.iearn.org">iEARN-USA</a></em>
 		<span>Video tutorial website for the iEARN Collaboration Centre. I designed the website and completed the front-end and WordPress back-end development.</span>
 		<span class="project--links"><a href="http://tutorials.iearn.org" title="iEARN CC Tutorials">Visit site</a>.</span>
 	</p>
