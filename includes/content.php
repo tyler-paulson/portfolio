b8d28d680aaa220216d06d28a9633fcead7d19ef
Tyler Paulson <me@tylerpaulson.com>
2014-10-20 16:38:33 -0400

    Adds iEARN CC Tutorials project

--- includes/content.php
+++ includes/content.php
@@ -1,11 +1,11 @@
 <?php
 	$projects = array(
+		array('title' => 'iEARN CC Tutorials','slug' => 'tutorials'),
 		array('title' => 'Miles a Minute','slug' => 'milesaminute'),
 		array('title' => 'Mogiv','slug' => 'mogiv'),
 		array('title' => 'The Life','slug' => 'thelife'),
 		array('title' => 'San Diego Half Marathon','slug' => 'sdhm'),
 		array('title' => 'In Deed And Truth','slug' => 'indeedandtruth'),
-		array('title' => 'Teed: Portraits of [South] Sudan','slug' => 'teed'),
-		array('title' => 'Son, I Loved You At Your Darkest','slug' => 'silyayd')
+		array('title' => 'Teed: Portraits of [South] Sudan','slug' => 'teed')
 	);
 ?>
---
+++ includes/projects/tutorials.php
@@ -0,0 +1,11 @@
+<div class="project" id="milesaminute">
+	<h2>iEARN CC Tutorials</h2>
+	<p>
+		<em class="project--brief">Completed as an employee of<a href="http://us.iearn.org">iEARN-USA</a></em>
+		<span>Video tutorial website for the iEARN Collaboration Centre. I designed the website and completed the front-end and WordPress back-end development.</span>
+		<span class="project--links"><a href="http://tutorials.iearn.org" title="iEARN CC Tutorials">Visit site</a>.</span>
+	</p>
+	<div class="project--image">
+		<img src="//s3.amazonaws.com/tylerpaulson.com/images/portfolio/tutorials-1.png" width="1216" height="924" alt="Homepage">
+	</div>
+</div>
