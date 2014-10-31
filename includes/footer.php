10ddfae82b42936a514a645f607bd4b0e8978db3
Tyler Paulson <me@tylerpaulson.com>
2014-10-31 15:35:56 -0400

    Checks if PHP splash variable is set

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
 
 	<script>
 		
-		var splash = <?php if($splash) { echo 'true'; } else { echo 'false'; } ?>;
+		var splash = <?php if(isset($splash) && $splash == true) { echo 'true'; } else { echo 'false'; } ?>;
 		
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
