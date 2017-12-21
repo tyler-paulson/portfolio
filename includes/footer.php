cfdd130309996d2ed05bf755af60227915723a1f
Tyler Paulson <me@tylerpaulson.com>
2017-12-20 22:54:07 -0500

    We incorporated.

--- includes/footer.php
+++ includes/footer.php
@@ -1,18 +1,18 @@
 
 	</div></article>
-	
-	<footer class="footer"><div class="wrap"><small>&copy; 2010&ndash;<?php echo date('Y'); ?> Tyler Paulson | <span class="nowrap">Hosted by <a href="https://gridandarrow.com">Grid & Arrow</a></span></small></div></footer>
-	
+
+	<footer class="footer"><div class="wrap"><small>&copy; 2010&ndash;<?php echo date('Y'); ?> Tyler Paulson Design &amp; Development, Inc | <span class="nowrap">Hosted by <a href="https://gridandarrow.com">Grid & Arrow, LLC</a></span></small></div></footer>
+
 	<?php if($_SERVER['SERVER_PORT'] != '8888') { ?><script>
-		
+
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		
+
 		ga('create', 'UA-19764958-1', 'tylerpaulson.com');
 		ga('send', 'pageview');
-		
+
 	</script><?php } ?>
 
 </body>
